ecd490645432d1467d0d8f46541cb3ae542743e9
vidyouth <vidyuthb@gmail.com>
2026-06-04 10:26:41 +0530

    Hardcode API URL in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'import.meta.env.VITE_API_URL': JSON.stringify('https://quill-backend-90qe.onrender.com'),
+  },
 })
